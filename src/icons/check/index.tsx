8198f816691c0509983c1bf5728f827bbfb4abde
Jose Arias <developstuff93@gmail.com>
2023-01-26 04:57:21 -0500

    add icons

---
+++ src/icons/check/index.tsx
@@ -0,0 +1,18 @@
+const CheckIcon = () => (
+  <svg
+    width="13"
+    height="9"
+    viewBox="0 0 13 9"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M1.50311 4.22L4.83645 8L11.5031 1"
+      stroke="white"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+  </svg>
+);
+export default CheckIcon;
