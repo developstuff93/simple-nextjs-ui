ade0f81b41430a1e22f40b8a0ca5365f87d913d7
Jose Arias <developstuff93@gmail.com>
2023-01-26 04:56:36 -0500

    tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        dark: "#263446",
+        light: "#9DA7BE",
+        green: "#70C4BB",
+        transparent: "#EFF1F5",
+      },
+    },
+  },
+  plugins: [],
+};
